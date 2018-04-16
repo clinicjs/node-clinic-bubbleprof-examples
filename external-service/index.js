481a2c101f5b22a979b3dfc9f83b46fa7db75287
Mathias Buus <mathiasbuus@gmail.com>
2018-04-16 15:15:29 +0200

    print info about external service

--- external-service/index.js
+++ external-service/index.js
@@ -1,6 +1,9 @@
 const http = require('http')
 const get = require('simple-get')
 
+console.log('Spin up the external service if you haven\'t:')
+console.log('node ./external.js --latency=some-latency-in-ms')
+
 const server = http.createServer(function (req, res) {
   const id = req.url.slice(1)
   get.concat(`http://localhost:3001/${id}/user.json`, function (err, _, body) {

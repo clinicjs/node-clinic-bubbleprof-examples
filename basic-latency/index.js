19b543acd4faa27f8024eb8585ee6b899d7d2d95
Mathias Buus <mathiasbuus@gmail.com>
2018-04-20 16:00:54 +0200

    breakup basic latency in multiple lines

--- basic-latency/index.js
+++ basic-latency/index.js
@@ -2,7 +2,10 @@ const http = require('http')
 
 const server = http.createServer(function (req, res) {
   // one bubble for the server request
-  setTimeout(() => res.end(), 1000) // 1s latency, increase 1s for longer lines, tiny bubble for .end()
+  setTimeout(function () {
+    // 1s latency, increase 1s for longer lines, tiny bubble for .end()
+    res.end()
+  }, 1000)
 })
 
 server.listen(10000)

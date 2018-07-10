79e6701cca9073b2bbb62438bc365f443a3bc3f1
Robb Dempsey <robb.dempsey@gmail.com>
2018-07-10 06:36:25 -0500

    [stories] hit the event-loop issue

---
+++ server-side-rendering/stories-with-slow-event-loop.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const autocannon = require('autocannon')
+
+const url = 'http://localhost:3000'
+
+const headers = {
+  'content-encoding': 'gzip',
+  'content-type': 'application/json; charset=utf-8'
+}
+const requests = [
+  {
+    method: 'GET',
+    path: '/show?filter=top&dataPath=slowEventLoop',
+    headers
+  }
+]
+
+const instance = autocannon({
+  title: 'ssr-example',
+  url,
+  connections: 10,
+  pipelining: 1,
+  duration: 10,
+  requests,
+}, console.log)
+
+process.once('SIGINT', () => {
+  instance.stop()
+})
+
+autocannon.track(instance)
